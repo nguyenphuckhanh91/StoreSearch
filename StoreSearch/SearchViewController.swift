b9bc17a82d8d57aaeae08e90110d67c844e71d75
nguyenphuckhanh91 <nguyenphuckhanh91@gmail.com>
2018-07-18 22:53:07 +0700

    Init Project

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  StoreSearch
 //
 //  Created by Khanh Nguyen on 7/18/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
